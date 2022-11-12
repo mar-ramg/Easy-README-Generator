5bf65e0972c6539a6bfc4c85bbe814c36e7711b1
who cares <mariaaramirez80@gmail.com>
2022-11-12 12:53:33 -0800

    added more code in mardown.js

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,20 +1,102 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to return a message if user doesn't want contributors
+function renderContributingSection(confirmContributers, data) {
+    if (!confirmContributers) {
+      return `
+    Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+      `;
+    } else {
+      return `
+    ${data}
+      `;
+    }
+  }
+  
+  // Function that returns a license badge based on which license is passed in
+  // If there is no license, return an empty string
+  function renderLicenseBadge(license) { 
+    if (license !== 'no license') {
+      return `
+    ![badge](https://img.shields.io/badge/license-${license}-blue)
+      `;
+    } else {
+      return ' ';
+    }
+  }
+  
+  // Function that returns the license link
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+  }
+  
+  // Function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+   }
+  
+   // Function that returns license in table of contents
+   // If there is no license, return an empty string
+  function renderLicenseTOC(license) {
+    if (license !== 'no license') {
+    return `
+    * [License](#license)
+      `;
+    } else {
+      return ' ';
+    }
+   }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+
+  ## Description:
+  ${data.description}
+
+  ## Installation:
+  ${data.installation}
+
+  ## Usage:
+  ${data.usage}
+
+  ## License:
+  ${data.licensing}
+
+  ## Contribution:
+  ${data.contribution}
+
+  ## Testing:
+  ${data.testing}
+
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} `;
 }
 
 module.exports = generateMarkdown;
